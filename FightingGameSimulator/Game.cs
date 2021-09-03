fda45f2a6c00e8ed0a7b5088dac3fbfb45a1b90d
Will <montero_w@live.com>
2021-09-03 11:50:54 -0500

    Completed Update Exercise

--- FightingGameSimulator/Game.cs
+++ FightingGameSimulator/Game.cs
@@ -21,6 +21,7 @@ namespace FightingGameSimulator
         Monster backUpWumpus;
         //Monste 4
         Monster unclePhil;
+
         bool gameOver = false;
         Monster currentMonster1;
         Monster currentMonster2;
@@ -38,7 +39,7 @@ namespace FightingGameSimulator
         }
 
       
-
+        // Prints Monster Stats 
         void PrintStatStruck(Monster monster)
         {
             Console.WriteLine("Name: " + monster.name +
@@ -48,12 +49,13 @@ namespace FightingGameSimulator
             Console.ReadKey();
         }
 
-        // Set Monsters To Attack Eachother
+        // Dmaage Calculations Between Enemies and there Defence 
         float CalculateDamage(Monster attack, Monster defender )
         {
             return attack.attack - defender.defense;
         }
 
+        // Simulats The Fighting Encounter
         float Fight(Monster attacker, ref Monster defender)
         {
             float damagetaken = CalculateDamage(attacker, defender);
@@ -75,12 +77,14 @@ namespace FightingGameSimulator
                 //Monster 1 Attacks Monster 2
                 float damagetaken = Fight(monster1, ref monster2);
                 monster1.health -= damagetaken;
+
                 // Displaying Damage taken to Monster1
                 Console.WriteLine(monster1.name + " Has Taken " + monster1.health + " Damage");
 
                 // Damage Calculation Between Monster 1 Attacking Monster 2  
                 damagetaken = Fight(monster2, ref monster1);
                 monster2.health -= damagetaken;
+
                 // Displaying Damage taken to Monster2
                 Console.WriteLine(monster2.name + " Has Taken " + monster2.health + " Damage");
 
@@ -105,10 +109,45 @@ namespace FightingGameSimulator
             return matchResult;
 
         }
+        void Start()
+        {
 
+            wumpus.name = "Wumpus";
+            wumpus.health = 20.0f;
+            wumpus.defense = 5.0f;
+            wumpus.attack = 15.0f;
+
+
+            thwompus.name = "Thwompus";
+            thwompus.health = 15.0f;
+            thwompus.defense = 10.0f;
+            thwompus.attack = 15.0f;
+
+
+            backUpWumpus.name = "BackUp Wumpus";
+            backUpWumpus.health = 3.0f;
+            backUpWumpus.defense = 5.0f;
+            backUpWumpus.attack = 25.6f;
+
+
+            unclePhil.name = "Uncle Phil";
+            unclePhil.health = 1.0f;
+            unclePhil.defense = 0f;
+            unclePhil.attack = 1000000000f;
+
+            //Set starting figters 
+            currentMonster1 = GetMonster(currentNumberIndex);
+            currentNumberIndex++;
+            currentMonster2 = GetMonster(currentNumberIndex);
+            currentNumberIndex++;
+
+        }
         void Update()
         {
             Battle();
+
+            UpdateCurrentMonsters();
+            Console.ReadKey(true);
         }
 
         Monster GetMonster(int monsterIndex)
@@ -138,10 +177,14 @@ namespace FightingGameSimulator
             return monster;
         }
 
+        /// <summary>
+        /// Simulates one turn in the current monster fight
+        /// </summary>
         void Battle()
         {
             //Prints Monster 1 Stats 
             PrintStatStruck(currentMonster1);
+
             //prints Monster 2 Stats 
             PrintStatStruck(currentMonster2);
 
@@ -157,53 +200,7 @@ namespace FightingGameSimulator
             // Displaying Damage taken to Monster2
             Console.WriteLine(currentMonster2.name + " Has Taken " + currentMonster2.health + " Damage");
 
-
-            PrintStatStruck(currentMonster1);
-            //prints Monster 2 Stats 
-            PrintStatStruck(currentMonster2);
-
-
-            Console.ReadKey();
-        }
-
-        void UpdateCurrentMonsters()
-        {
-            
-        }
-
-        public void Run()
-        {
-            
-            wumpus.name = "Wumpus";
-            wumpus.health = 20.0f;
-            wumpus.defense = 5.0f;
-            wumpus.attack = 15.0f;
-
-            
-           
-            thwompus.name = "Thwompus";
-            thwompus.health = 15.0f;
-            thwompus.defense = 10.0f;
-            thwompus.attack = 15.0f;
-
-            
-            backUpWumpus.name = "BackUp Wumpus";
-            backUpWumpus.health = 3.0f;
-            backUpWumpus.defense = 5.0f;
-            backUpWumpus.attack = 25.6f;
-
-            
-            unclePhil.name = "Uncle Phil";
-            unclePhil.health = 1.0f;
-            unclePhil.defense = 0f;
-            unclePhil.attack = 1000000000f;
-
-
-            Console.ReadKey();
-            //Console.Clear();
-
-
-            string resault = StartBattle(ref wumpus, ref thwompus);
+            string resault = StartBattle(ref currentMonster1, ref currentMonster2);
 
             if (resault == "Draw")
                 Console.WriteLine(resault + " Game");
@@ -212,6 +209,51 @@ namespace FightingGameSimulator
             else
                 Console.WriteLine(resault + " Is The Winner");
 
+            PrintStatStruck(currentMonster1);
+            //prints Monster 2 Stats 
+            PrintStatStruck(currentMonster2);
+
+
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Change one of the current figters to be the next in the list
+        /// if it has died. Ends rhe game if all figters in the list have been used.
+        /// </summary>
+        void UpdateCurrentMonsters()
+        {   
+            // If monster 1 died
+            if (currentMonster1.health <= 0)
+            {
+
+                // increment the current monster index and swap out the monster
+                currentNumberIndex++;
+                currentMonster1 = GetMonster(currentNumberIndex);
+            }
+            // If monster 2 died 
+            if (currentMonster2.health <= 0)
+            {
+                // increment the current monster index and swap out the monster
+                currentNumberIndex++;
+                currentMonster2 = GetMonster(currentNumberIndex);
+            }
+            // If its not ether of them or the index went out of bounds  
+            if (currentMonster2.name == "None" || currentMonster1.name == "None" && currentNumberIndex >= 4)
+            {
+                // ends game 
+                Console.WriteLine("Simulation Over");
+                gameOver = true;
+            }
+            
+        }
+
+        public void Run()
+        {
+            Start();
+            while (!gameOver)
+            {
+                Update();
+            }
         }
     }
 }
