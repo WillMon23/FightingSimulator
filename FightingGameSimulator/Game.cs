2e51de307336e10caeb050a20b1915a629b2b758
Will <montero_w@live.com>
2021-09-08 11:34:17 -0500

    Created UpdateScreen and created a few display menus Restart, Staring

--- FightingGameSimulator/Game.cs
+++ FightingGameSimulator/Game.cs
@@ -25,7 +25,8 @@ namespace FightingGameSimulator
         bool gameOver = false;
         Monster currentMonster1;
         Monster currentMonster2;
-        int currentNumberIndex = 1;
+        int currentNumberIndex = 0;
+        int currentScene = 0;
 
 
         //Calculates Damage Done 
@@ -37,7 +38,6 @@ namespace FightingGameSimulator
             return damage;
 
         }
-
       
         // Prints Monster Stats 
         void PrintStatStruck(Monster monster)
@@ -52,7 +52,10 @@ namespace FightingGameSimulator
         // Dmaage Calculations Between Enemies and there Defence 
         float CalculateDamage(Monster attack, Monster defender )
         {
-            return attack.attack - defender.defense;
+            float damage = attack.attack - defender.defense;
+            if (damage <= 0)
+                damage = 0;
+            return damage;
         }
 
         // Simulats The Fighting Encounter
@@ -132,24 +135,28 @@ namespace FightingGameSimulator
 
             unclePhil.name = "Uncle Phil";
             unclePhil.health = 1.0f;
-            unclePhil.defense = 0f;
+            unclePhil.defense = 10.0f;
             unclePhil.attack = 100f;
 
+            RestartCurrentMonsters();
+        }
+
+        void RestartCurrentMonsters()
+        {
+            currentNumberIndex = 0;
             //Set starting figters 
             currentMonster1 = GetMonster(currentNumberIndex);
             currentNumberIndex++;
             currentMonster2 = GetMonster(currentNumberIndex);
-            currentNumberIndex++;
-
         }
-        void Update()
-        {
-            Battle();
-
-            UpdateCurrentMonsters();
-            Console.ReadKey(true);
-        }
-
+        /// <summary>
+        /// A Function that mimmics a List 
+        /// cycling through the ammount of monster within hat list 
+        /// Once the numaration runs through the index it then
+        /// retuens the monster in that index
+        /// </summary>
+        /// <param name="monsterIndex"> Location of said needed Moster </param>
+        /// <returns></returns>
         Monster GetMonster(int monsterIndex)
         {
             Monster monster;
@@ -158,25 +165,183 @@ namespace FightingGameSimulator
             monster.defense = 1f;
             monster.health = 1f;
 
-            if(monsterIndex == 1)
+            if(monsterIndex == 0)
             {
                 monster = unclePhil;
             }
-            else if (monsterIndex == 2)
+            else if (monsterIndex == 1)
             {
                 monster = backUpWumpus;
             }
-            else if (monsterIndex == 3)
+            else if (monsterIndex == 2)
             {
                 monster = wumpus;
             }
-            else if (monsterIndex == 4)
+            else if (monsterIndex == 3)
             {
                 monster = thwompus;
             }
             return monster;
         }
 
+        /// <summary>
+        /// Change one of the current figters to be the next in the list
+        /// if it has died. Ends rhe game if all figters in the list have been used.
+        /// </summary>
+        void UpdateCurrentMonsters()
+        {   
+            // If monster 1 died
+            if (currentMonster1.health <= 0)
+            {
+
+                // increment the current monster index and swap out the monster
+                currentNumberIndex++;
+                currentMonster1 = GetMonster(currentNumberIndex);
+            }
+            // If monster 2 died 
+            if (currentMonster2.health <= 0)
+            {
+                // increment the current monster index and swap out the monster
+                currentNumberIndex++;
+                currentMonster2 = GetMonster(currentNumberIndex);
+            }
+            // If its not ether of them or the index went out of bounds  
+            if (currentMonster2.name == "None" || currentMonster1.name == "None" && currentNumberIndex >= 4)
+            {
+                // ends game 
+
+                currentScene = 2;
+            }
+            
+        }
+
+        void UpdateCurrentSceen()
+        {
+
+            switch (currentScene)
+            {
+                case 0:
+                    DisplayMainMenu();
+                    break;
+
+                case 1:
+                    Battle();
+                    UpdateCurrentMonsters();
+                    Console.ReadKey();
+                    break;
+
+                case 2:
+                    DisplayRestartMenu();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid scene index");
+                    break;
+                    
+
+            } 
+
+            if (currentScene == 0)
+                DisplayMainMenu();
+
+            else if (currentScene == 1)
+            {
+                Battle();
+                UpdateCurrentMonsters();
+                Console.ReadKey(true);
+            }
+
+            else if (currentScene == 2)
+                DisplayRestartMenu();
+        }
+
+
+        /// <summary>
+        /// Checkes if user wants to end or start over 
+        /// </summary>
+        void DisplayRestartMenu()
+        {
+            // Sets the users creats the end screen display to grab the users choice
+            int choice = GetInput("Simulation over. Would you like to play again?", "Yes", "No");
+
+            // checks if the user chooces 1... 
+            if (choice == 1)
+            {
+                /// ...Starts the all Over and sets the currentScene to 0
+                RestartCurrentMonsters();
+                currentScene = 0;
+            }
+            // checks if the user chooses 2...
+            else if (choice == 2)
+                //Ends the Game 
+                gameOver = true;
+        }
+
+        /// <summary>
+        /// Displays the starting menu. Gives the user the option to start or
+        /// exit the simulation 
+        /// </summary>
+        void DisplayMainMenu()
+        {
+            //Gets users choice
+            int choice = GetInput("Welcome to Monster Fight Simulator and Uncle Phill", "Start Simulation", "Quit Application");
+
+            //Wither the choice is to start the simulation 
+            if (choice == 1)
+            {
+                currentScene = 1;
+            }
+
+            //Wither the choice is to end the Simulation
+            else if (choice == 2)
+            {
+                gameOver = true;
+            }
+        }
+
+        /// <summary>
+        /// Gets users input based on some decision 
+        /// </summary>
+        /// <param name="description">The Context of the deciosion</param>
+        /// <param name="option1">sets the first writen option for the user</param>
+        /// <param name="option2">sets the second writen option for the user</param>
+        /// <param name="pauseInvaled"> if true user needs to press a key to continue</param>
+        /// <returns></returns>
+        int GetInput(string description, string option1, string option2, bool pauseInvaled = false)
+        {
+            // sets the context for the user & there choices 
+            Console.WriteLine(description);
+            Console.WriteLine("1. " + option1);
+            Console.WriteLine("2. " + option2);
+
+            //Gets the input
+            string input = Console.ReadLine();
+            int chocie = 0;
+
+            // if user types 1 
+            if (input == "1")
+                chocie = 1;
+
+            // if user types 2 
+            else if (input == "2")
+                chocie = 2;
+
+            // Makes sure user is not typing off screen response 
+            else
+            {
+                // Prints out invaled input
+                Console.WriteLine("Invaled Input");
+                
+                // checks to see if true 
+                if (pauseInvaled)
+                    // makes user type a Key to continue 
+                  Console.ReadKey(true);
+            }
+
+            return chocie;
+
+        }
+        
         /// <summary>
         /// Simulates one turn in the current monster fight
         /// </summary>
@@ -209,6 +374,7 @@ namespace FightingGameSimulator
             else
                 Console.WriteLine(resault + " Is The Winner");
 
+            //prints Monster 1 Stats
             PrintStatStruck(currentMonster1);
             //prints Monster 2 Stats 
             PrintStatStruck(currentMonster2);
@@ -216,35 +382,14 @@ namespace FightingGameSimulator
 
             Console.ReadKey();
         }
-        /// <summary>
-        /// Change one of the current figters to be the next in the list
-        /// if it has died. Ends rhe game if all figters in the list have been used.
-        /// </summary>
-        void UpdateCurrentMonsters()
-        {   
-            // If monster 1 died
-            if (currentMonster1.health <= 0)
-            {
 
-                // increment the current monster index and swap out the monster
-                currentNumberIndex++;
-                currentMonster1 = GetMonster(currentNumberIndex);
-            }
-            // If monster 2 died 
-            if (currentMonster2.health <= 0)
-            {
-                // increment the current monster index and swap out the monster
-                currentNumberIndex++;
-                currentMonster2 = GetMonster(currentNumberIndex);
-            }
-            // If its not ether of them or the index went out of bounds  
-            if (currentMonster2.name == "None" || currentMonster1.name == "None" && currentNumberIndex >= 4)
-            {
-                // ends game 
-                Console.WriteLine("Simulation Over");
-                gameOver = true;
-            }
-            
+        /// <summary>
+        /// Called Every Game Loop
+        /// </summary>
+        void Update()
+        {
+            UpdateCurrentSceen();
+            Console.Clear();
         }
 
         public void Run()
